0d74078e3a531740c0686883bb99681e3b72677e
bertam <bertamerelo@gmail.com>
2018-05-16 00:42:07 +0200

    Metod joc

--- src/com/trabajiwi/model/Juego.java
+++ src/com/trabajiwi/model/Juego.java
@@ -13,8 +13,28 @@ public class Juego {
 
     public Juego() {
         tablero = new Tablero(3,3);
+        do {
+            moverJugador(currentPlayer);
+            tablero.dibujar();
+            actualizarJuego(currentPlayer);
 
+            if (currentState == Estado.CROSSWON) {
+                System.out.println("'X' GANA!");
+            } else if (currentState == Estado.NOUGHTWON) {
+                System.out.println("'O' GANA!");
+            } else if (currentState == Estado.DRAW) {
+                System.out.println("EMPATE!");
+            }
+            // cambiem jugador
+            currentPlayer = (currentPlayer == TipoFicha.CROSS) ? TipoFicha.NOUGHT : TipoFicha.CROSS;
+        } while (currentState == Estado.PLAYING);
         initGame();
+    }
+
+    private void actualizarJuego(TipoFicha currentPlayer) {
+    }
+
+    private void moverJugador(TipoFicha currentPlayer) {
     }
 
     public void initGame() {
