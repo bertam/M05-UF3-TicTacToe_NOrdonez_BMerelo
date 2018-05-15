2f488c4e79eed961e6013d2ef382b5713d2a25c9
Nil Ordoñez <nilordonez.7@gmail.com>
2018-05-16 00:50:58 +0200

    Juego implementado.

--- src/com/trabajiwi/model/Juego.java
+++ src/com/trabajiwi/model/Juego.java
@@ -32,14 +32,39 @@ public class Juego {
     }
 
     private void actualizarJuego(TipoFicha currentPlayer) {
+        if (tablero.haGanado(currentPlayer)) {  // check for win
+            currentState = (currentPlayer == TipoFicha.CROSS) ? Estado.CROSSWON : Estado.NOUGHTWON;
+        } else if (tablero.esEmpate()) {  // check for draw
+            currentState = Estado.DRAW;
+        }
     }
 
     private void moverJugador(TipoFicha currentPlayer) {
+        boolean validInput = false;  // for validating input
+        do {
+            if (currentPlayer == TipoFicha.CROSS) {
+                System.out.print("Jugador 'X', introduce tu movimiento (fila[1-3] columna[1-3]): ");
+            } else {
+                System.out.print("Player 'O', introduce tu movimiento (fila[1-3] columna[1-3]): ");
+            }
+            int fila = in.nextInt() - 1;
+            int columna = in.nextInt() - 1;
+            if (fila >= 0 && fila < 3 && columna >= 0 && columna < 3
+                    && tablero.getCasillas()[fila][columna].getTipoFicha() == TipoFicha.EMPTY) {
+                tablero.getCasillas()[fila][columna].setTipoFicha(currentPlayer);
+                tablero.setFilaActual(fila);
+                tablero.setColumnaActual(columna);
+                validInput = true;
+            } else {
+                System.out.println("Este movimiento en (" + (fila + 1) + "," + (columna + 1)
+                        + ") no es valido. Vuelve a insertar");
+            }
+        } while (!validInput);
     }
 
     public void initGame() {
-        tablero.init();  // clear the board contents
-        currentPlayer = TipoFicha.CROSS;       // CROSS plays first
-        currentState = Estado.PLAYING; // ready to play
+        tablero.init();
+        currentPlayer = TipoFicha.CROSS;
+        currentState = Estado.PLAYING;
     }
 }
