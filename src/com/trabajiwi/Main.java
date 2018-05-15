5a5fb048fdf09e96a0407faa79e4daa81d60cadc
Nil Ordoñez <nilordonez.7@gmail.com>
2018-05-16 00:54:17 +0200

    Run game.

--- src/com/trabajiwi/Main.java
+++ src/com/trabajiwi/Main.java
@@ -1,8 +1,10 @@
 package com.trabajiwi;
 
+import com.trabajiwi.model.Juego;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        new Juego().run();
     }
 }
